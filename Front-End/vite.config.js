a086f8a3280a114ff0d2b3e7cd09fbcbc3935369
Abdulrahman-Algaadi <bdalrhmnaljdy395@gmail.com>
2026-03-28 15:33:54 +0300

    fix(backend): fix profile update userId bug and configure frontend vite target proxy

--- Front-End/vite.config.js
+++ Front-End/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://online-store-yemen.vercel.app',
         changeOrigin: true,
         secure: false,
       },
